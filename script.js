0030e161dbdbbb10c7f4a02b460dab143b5164b5
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-05-10 10:34:47 +0200

    ee

--- script.js
+++ script.js
@@ -13,47 +13,76 @@ console.log(paperino.toString());
 const paperina = new DeadLineToDo('manda mememel', new Date(2022, 6, 9), ToDo.PRIORITY.Max, ['tag4']); 
 console.log(paperina.toString()); 
 
+const toDoTemplate = ` 
+<div class="todo-container"> 
+    <div class="task-div"> 
+        <div class="task-griglia"> 
+            <span class = "titolo-container">#TODONAME</span> 
+            <div class="task-flex"> 
+
+            </div> 
+            <div id='date-container' class="creation-container"> 
+                    Da: #CREATIONDATE  
+            </div> 
+            <div class = "button-container">
+                <button class="button">COMPLETATO</button> 
+            </div>
+        </div> 
+     </div>
+</div> 
+` 
+
+const doneTemplate = ` 
+<div class="todo-container"> 
+    <div class="task-div"> 
+        <div class="task-griglia"> 
+            <span class = "titolo-container">#TODONAME</span> 
+            <div class="task-flex"> 
+
+            </div> 
+            <div id='date-container' class="creation-container"> 
+                    Da: #CREATIONDATE  
+            </div> 
+        </div> 
+     </div>
+</div> 
+` 
+
 //  metto null perché va rispettato ordine di proprietà; attingerà a valore default;
 // const topolino = new DeadLineToDo('compra pane', null, ToDo.PRIORITY.Medium, ['tag5']);
 // console.log(topolino.toString()); 
 
-const toDoList = [todo1, todo2, paperino, paperina];  
+let toDoList = [todo1, todo2, paperino, paperina];  
 
-function displayToDoWithTemplate() {
-    const template = ` 
-    <div class="todo-container"> 
-        <div class="task-div"> 
-            <div class="task-griglia"> 
-                <span class = "titolo-container">#TODONAME</span> 
-                <div class="task-flex"> 
+const doneList = []; 
 
-                </div> 
-                <div id='date-container' class="creation-container"> 
-                        Da: #CREATIONDATE  
-                </div> 
-                <div class = "button-container">
-                    <button class="button">COMPLETATO</button> 
-                </div>
-            </div> 
-         </div>
-    </div> 
+function displayToDoWithTemplate(template, containerName, toDoArray) {
 
-    <div id="done-container"> 
+    const mainContainer = document.getElementById(containerName); 
 
-    </div>
-    ` 
-    const toDoListContainer = document.getElementById('todo-list-container'); 
+    mainContainer.innerHTML = '';
 
-    for (let i = 0; i < toDoList.length; i++) {
-        const todo = toDoList[i]; 
+    for (let i = 0; i < toDoArray.length; i++) {
+        const todo = toDoArray[i]; 
         const div = document.createElement('div'); 
         
         const toDoTemplate = template.replace('#TODONAME', todo.name) 
                                      .replace('#CREATIONDATE', todo.creationDate.toISOString()); 
                 
         div.innerHTML = toDoTemplate; 
-        toDoListContainer.appendChild(div); 
+        mainContainer.appendChild(div); 
 //  aggiungo elementi unici;  
+
+        const doneButton = div.querySelector('.button');  
+//  richiamo bottone;
+        if (doneButton) { 
+//  se bottone presente, avvio funzioni;
+            doneButton.style.backgroundColor = todo.priority.color; 
+            doneButton.onclick = () => removeDoneToDo(todo); 
+        }
+  
+//      gli ho passato lambda;
+//  quando passo formula in javascript, lo faccio senza parentesi; 
 
 //      per cambiare colore in base a priorità
         const toDoContainer = div.querySelector('.task-div'); 
@@ -79,10 +108,94 @@ function displayToDoWithTemplate() {
     }
 } 
 
-displayToDoWithTemplate();
+// displayToDoWithTemplate(); 
 
-// const doneList = [];
 
-//  uno degli styles dipende dalla priority (bg-color, in base a priority); 
-//  cercare come dare istruzioni stile a file js. 
-//  aggiungo style.bgcolor inline a html;
+// function displayDoneWithTemplate() {
+//     const template = ` 
+//     <div class="todo-container"> 
+//         <div class="task-div"> 
+//             <div class="task-griglia"> 
+//                 <span class = "titolo-container">#TODONAME</span> 
+//                 <div class="task-flex"> 
+
+//                 </div> 
+//                 <div id='date-container' class="creation-container"> 
+//                         Da: #CREATIONDATE  
+//                 </div> 
+//             </div> 
+//          </div>
+//     </div> 
+
+//     <div id="done-container"> 
+
+//     </div>
+//     ` 
+//     const doneContainer = document.getElementById('done-container'); 
+
+//     doneContainer.innerHTML = ''
+
+//     for (let i = 0; i < doneList.length; i++) {
+//         const todo = doneList[i]; 
+//         const div = document.createElement('div'); 
+        
+//         const toDoTemplate = template.replace('#TODONAME', todo.name) 
+//                                      .replace('#CREATIONDATE', todo.creationDate.toISOString()); 
+                
+//         div.innerHTML = toDoTemplate; 
+//         doneContainer.appendChild(div); 
+// //  aggiungo elementi unici;  
+
+//         if (todo.deadLine) {
+//             // const dateContainer = document.getElementsByClassName('creation-container')[0]; 
+//             const dateContainer = div.querySelector('.creation-container');
+//             const dateSpan = document.createElement('div'); 
+//             const dateNode = document.createTextNode('a: ' + todo.deadLine.toISOString()); 
+//             dateSpan.appendChild(dateNode); 
+//             dateContainer.appendChild(dateSpan);
+//         } 
+
+//         const tagContainer = div.querySelector('.task-flex');
+//         for (const tag of todo.tags) {
+//             const tagSpan = document.createElement('div');  
+//             const node = document.createTextNode(tag); 
+//             tagSpan.classList.add('tags-items')
+//             tagSpan.appendChild(node); 
+//             tagContainer.appendChild(tagSpan);
+//         }
+//     }
+// } 
+
+displayToDoWithTemplate(toDoTemplate, 'todo-list-container', toDoList);
+
+function removeDoneToDo(todo){
+    toDoList = toDoList.filter(t => t.name !== todo.name); 
+    displayToDoWithTemplate(toDoTemplate, 'todo-list-container', toDoList); 
+    doneList.push(todo); 
+    displayToDoWithTemplate(doneTemplate, 'done-container', doneList);
+} 
+
+function orderByName(){ 
+    toDoList.sort(compareByName); 
+    displayToDoWithTemplate(toDoTemplate, 'todo-list-container', toDoList); 
+    doneList.sort(compareByName); 
+    displayToDoWithTemplate(doneTemplate, 'done-container', doneList);
+} 
+
+function compareByName(todo1, todo2) {
+    return todo1.name.localeCompare(todo2.name); 
+} 
+
+function orderByDate() {
+    toDoList.sort(compareByDate); 
+    displayToDoWithTemplate(toDoTemplate, 'todo-list-container', toDoList); 
+    doneList.sort(compareByDate); 
+    displayToDoWithTemplate(doneTemplate, 'done-container', doneList);
+} 
+
+function compareByDate(todo1, todo2) {
+    return todo1.creationDate.getTime() - todo2.creationDate.getTime(); 
+} 
+
+const dateButton = document.getElementById('date-order-btn'); 
+dateButton.onclick = orderByDate;
